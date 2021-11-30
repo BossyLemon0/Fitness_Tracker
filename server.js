f34fa2079041ad90ec3b2eca741f334b984125dc
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-11-29 23:54:51 -0600

    This commit adds the creation of the server file.

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const routes = require('./controllers');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Port open on ${PORT}`);
+});
